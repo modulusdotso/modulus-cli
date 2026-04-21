ddcc31aaac70f51473da1f2ecaa63ad39481abaf
0verread <iamsubhajit.d@gmail.com>
2026-04-21 16:40:03 -0700

    minor fix: changed to https from http

--- src/modulus_cli/constants.py
+++ src/modulus_cli/constants.py
@@ -1,3 +1,3 @@
-MODULUS_BASE_URL = "http://eve.backtickai.com"
+MODULUS_BASE_URL = "https://eve.backtickai.com"
 MODULUS_INDEX_URL = f"{MODULUS_BASE_URL}/v1/index/inventory"
 MODULUS_API_KEY_VERIFY_URL = f"{MODULUS_BASE_URL}/v1/auth/apikey/verify"
